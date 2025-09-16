6c047ff57dcf356fc591863ea0a1b22ae57fcc6f
nikhil-p17 <nikhilpatil17690@gmail.com>
2025-09-16 19:24:57 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+
+let age = 12345;
+let userEmail = "nikhil@gmail.com";
+var userPassword = "qwerty";
+let isLoggedIn = true;
+let userState;
+
+
+
+// Primitive Datatypes:
+
+// number ==> 2 to power 53
+// bigint
+// string ==> text
+// boolean ==> true/false
+// null ==> standalone value (empty value) ==> It is a object
+// undefined
+
+
+// object
+
+console.log(typeof null);
+
+//console.table(typeof[userId, userEmail, userPassword, userCity]);
