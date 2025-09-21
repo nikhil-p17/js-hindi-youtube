a75a65e6b8a6d2ad95ce578acb4c12d3d0dd8c44
nikhil-p17 <nikhilpatil17690@gmail.com>
2025-09-21 18:37:04 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,9 +1,49 @@
-let score = "35";
+
+
+let score = "35abc";
+let age = null;
+
+
 
 console.log(typeof score);
-console.log(typeof (score));
+console.log((score));
+
+//convert to Number
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let valueInNum = Number(age);
+
+console.log(typeof valueInNum);
+console.log(valueInNum);
 
 
-let valueIsNumber = Number(score);
+// "35" => 35
+// "35abc" => NaN
+// "null" => 0
+// "true/false" => 1/0
+// "undefined" => NaN
 
-console.log(typeof valueIsNumber);
+
+//convert to Boolean
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(booleanIsLoggedIn);
+
+// "1" => true; "0" => false;
+// "" => false;
+// "Nikhil" => true;
+
+
+//convert to String
+let anyNumber = 35;
+
+let valueInString = String(anyNumber);
+
+
+console.log(valueInString);
+console.log(typeof valueInString);
